4ee525a29c03174785d079913fb79e33723a61eb
kaoutar <kaoutar@gmail.com>
2026-02-24 15:11:50 +0000

    crrer fnc qui ajoute dynamq un container avec des info de api info

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,1 +1,34 @@
 //  Fonctions d'affichage des listes et modales
+
+import { getAllRecipes } from '../api/recipeProvider.js';;
+
+
+export async function displayRecipes() {//had fnc pour aff rcip via api
+
+  const container = document.getElementById("recipes-container");//container li f index
+
+  const recipes = await getAllRecipes();
+
+  container.innerHTML = ""; // pour vider container
+
+  recipes.forEach(recipe => {
+
+    const card = document.createElement("div");
+    card.style.border = "1px solid #ccc";
+    card.style.margin = "10px";
+    card.style.padding = "10px";
+
+    const img = document.createElement("img");
+    img.src = recipe.image;
+    img.style.width = "150px";
+
+    const title = document.createElement("h3");
+    title.textContent = recipe.name;
+
+    card.appendChild(img);
+    card.appendChild(title);
+
+    container.appendChild(card);
+  });
+}
+
