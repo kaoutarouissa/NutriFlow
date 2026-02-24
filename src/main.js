b6a15bcf173bcf10779defff0cc8860a645f9264
kaoutar <kaoutar@gmail.com>
2026-02-24 15:13:10 +0000

    imporet les fnc pour afich recipes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 // Point d'entrée : orchestre l'allumage de l'app
+import { getAllRecipes} from './api/recipeProvider.js';
+import { displayRecipes } from './ui/render.js';
+
+const btn = document.getElementById("introbtn");// puis index lie direct m3a main dert event btn
+
+btn.addEventListener("click", () => {
+  displayRecipes();
+});
+async function initApp() {//had fnc pour test fn getrec li katjib data mn api
+    try {
+        const recipes = await getAllRecipes(); // NJIB data mn getAllRecipes 
+        
+        console.log("tester Data mni jat:", recipes)             // pour test f console
+    } catch (error) {
+        console.error("Erreur d'affichage :", error);
+    }
+}
+
+initApp();
