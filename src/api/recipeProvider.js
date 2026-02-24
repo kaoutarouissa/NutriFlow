073f8d4276ab9d9db8046c39e3ba45fa30c99d1f
kaoutar <kaoutar@gmail.com>
2026-02-24 15:10:11 +0000

    creer fnc pour recois les datat api

--- src/api/recipeProvider.js
+++ src/api/recipeProvider.js
@@ -1,1 +1,13 @@
 //  Fonctions fetch() : getAllRecipes, searchRecipes (appel interier)
+  export async function getAllRecipes() {//asunc travail with fnc prend temp pour exect
+  try {
+    const response = await fetch('https://dummyjson.com/recipes');
+    const data = await response.json();//reponse sous forme de json
+return data.recipes; 
+    
+    // console.log("tester my data de api",data);
+
+  } catch (error) {
+    console.error('thas is catch Error:', error);
+  }
+}
