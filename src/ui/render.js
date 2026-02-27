44bda9ba58d0b6c6a20e55efa2607c832a3f2c78
kaoutar <kaoutar@gmail.com>
2026-02-27 13:50:20 +0000

    fixing style scss and responsiv

--- src/ui/render.js
+++ src/ui/render.js
@@ -3,23 +3,26 @@ export function displayRecipes(recipes) {
   const recipeAffiche=document.getElementById("recipeSearch")
  const heading = document.getElementById("titleh2");
  const searchInput = document.getElementById("Input");
- searchInput.placeholder = "Search recipes...";
-heading.textContent = "Our Healthy Recipes";
+ searchInput.placeholder = "I would like to cook...";
+heading.textContent = "Find best recipes for cooking";
   recipes.forEach((recipe) => {
     const card = document.createElement("div");
-    card.style.border = "1px solid #000";
-    card.style.margin = "10px";
-    card.style.padding= "10px";
+    card.classList.add("recipe-card");
 
-    const img = document.createElement("img");
-    img.src = recipe.image;
-    img.style.width = "150px";
+    card.innerHTML = `<div class="div1">
+      <h3 class="recipe-title">${recipe.name}</h3>
+      <div class="div2">
+              <img class="recipe-img" src="../images/search.png" alt="">
+        <img class="recipe-img" src="../images/search.png" alt="">
+        </div>
+        </div>
 
-    const title = document.createElement("h3");
-    title.textContent = recipe.name;
 
-    card.appendChild(img);
-    card.appendChild(title);
+        <div class="img">
+            <img class="icon-share" src="${recipe.image}" alt="${recipe.image}">
+          </div>
+        
+    `;
+
     container.appendChild(card);
-  });
-}
+})}
